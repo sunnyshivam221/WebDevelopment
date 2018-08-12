2bfc5cb55452de320adc5c0ef8bcfb17665998f1
SHIVAM KUMAR <sunnyshivam221@gmail.com>
2018-08-12 23:54:51 +0530

    adding massasing features

--- WebSocket/frontend/script.js
+++ WebSocket/frontend/script.js
@@ -11,14 +11,28 @@ window.onload=function(){
     let btn=this.document.getElementById('btn')
     let massage=document.getElementById('massage')
     let list=document.getElementById('list')
+    let loginbtn=this.document.getElementById('loginbtn')
+    let chatbox=document.getElementById('chatbox')
+    let loginbox=this.document.getElementById('loginbox')
+    let username=document.getElementById('username')
+
+    let user=''
 
     btn.onclick=function(){
-        socket.emit('send_msg',{msg:massage.value})
+        socket.emit('send_msg',{user:user,msg:massage.value})
+    }
+
+    loginbtn.onclick=function(){
+        // loginbox.hide();
+        // chatbox.show();
+        user=username.value;
+        loginbox.style.display='none'
+        chatbox.style.display='block'
     }
 
     socket.on('Recieved_msg',function(data){
         let newList=document.createElement('li')
-        newList.innerText=data.msg;
+        newList.innerText=`${data.user} : ${data.msg}`;
         list.appendChild(newList);
     })
 }
